44231a142ab20ea1b04a4b649f04bc7f3d980ee9
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-03-31 16:39:14 +0300

    add

---
+++ BookShop/Book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+namespace Library
+{
+    public partial class Book : Form
+    {
+        static SpeechSynthesizer synth;
+        //string fPath;
+        public Book()
+        {
+            InitializeComponent();
+            synth = new SpeechSynthesizer();
+            //textBox1.Text = File.ReadAllText(@"global::Library\Properties\Resources\book2.txt");
+            synth.SetOutputToDefaultAudioDevice();
+            synth.SpeakCompleted += Synth_SpeakCompleted;
+        }
+        public Book(string str, bool b = false)
+        {
+            InitializeComponent();
+            synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            synth.SpeakCompleted += Synth_SpeakCompleted;
+            if (b) { textBox1.Text = File.ReadAllText(str); }
+            //else { textBox1.Text = File.ReadAllText(FullPath(str)); }
+            else
+            { 
+                string[] lns = File.ReadAllLines(FullPath(str));
+                for(int i = 0; i < 10 ; i++)
+                {
+                    textBox1.Text += lns[i];
+                }
+            }
+        }
+        private static string FullPath(string str)
+        { 
+            string workDir = Environment.CurrentDirectory;
+            string projDir = Directory.GetParent(workDir).Parent.FullName;
+            return Path.Combine(projDir, str);
+        }
+        private void Synth_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            MessageBox.Show("Speech end", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = File.ReadAllText(@"global::Library.Properties.Resources.1111111");
+            if (!textBox1.Text.Equals(String.Empty)) { synth.SpeakAsync(textBox1.Text); }
+            if (!textBox2.Text.Equals(String.Empty)) { synth.SpeakAsync(textBox2.Text); }
+        }
+    }
+}
--- BookShop/Form1.cs
+++ BookShop/Form1.cs
@@ -8,13 +8,146 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BookShop
+namespace Library
 {
-    public partial class Form1 : Form
+    public partial class Polka : Form
     {
-        public Form1()
+        
+        Book book;
+        string filePath;
+        List<Image> picts;
+        Random rand = new Random();
+        public Polka()
         {
             InitializeComponent();
+            radioButton1.Checked = true;
+            pictureBox11.Visible = false;
+            picts = new List<Image>() { global::BookShop.Properties.Resources.к1,
+                global::BookShop.Properties.Resources.к2,
+                global::BookShop.Properties.Resources.к3,
+                global::BookShop.Properties.Resources.к4,
+                global::BookShop.Properties.Resources.к5,
+                global::BookShop.Properties.Resources.к6
+            };
         }
+        private bool ReadOrDel()
+        {
+            if (radioButton1.Checked == true) return true;
+            else return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox11.Image = picts[rand.Next(0,5)];
+            using(OpenFileDialog ofd = new OpenFileDialog()) 
+            {
+                ofd.Filter = "TEXT|*.txt|ALL|*.*";
+                ofd.Title = "Choose";
+                if(ofd.ShowDialog() == DialogResult.OK) { filePath = ofd.FileName; }
+            }
+            pictureBox11.Visible = true;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book(@"Resources\book1.txt");
+                book.ShowDialog();
+            }
+            else { pictureBox1.Visible = false; }
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book(@"Resources\book2.txt");
+                book.ShowDialog();
+            }
+            else { pictureBox2.Visible = false; }
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox3.Visible = false; }
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox4.Visible = false; }
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox5.Visible = false; }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox6.Visible = false; }
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox7.Visible = false; }
+        }
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox8.Visible = false; }
+        }
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox9.Visible = false; }
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book();
+                book.ShowDialog();
+            }
+            else { pictureBox10.Visible = false; }
+        }
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            if (ReadOrDel())
+            {
+                book = new Book(filePath,true);
+                book.ShowDialog();
+            }
+            else { pictureBox11.Visible = false; }
+        }
+
     }
 }
